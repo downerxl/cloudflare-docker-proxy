589dbd62f60931aa5a05ca80bbb6c743e716bb87
周晓亮 <zhouxl@jiyitech.com>
2024-07-01 10:54:56 +0800

    1. use my domain

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.jiyitech.top": dockerHub,
+  "quay.jiyitech.top": "https://quay.io",
+  "gcr.jiyitech.top": "https://gcr.io",
+  "k8s-gcr.jiyitech.top": "https://k8s.gcr.io",
+  "k8s.jiyitech.top": "https://registry.k8s.io",
+  "ghcr.jiyitech.top": "https://ghcr.io",
+  "cloudsmith.jiyitech.top": "https://docker.cloudsmith.io",
+  "ecr.jiyitech.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.jiyitech.top": dockerHub,
 };
 
 function routeByHosts(host) {
